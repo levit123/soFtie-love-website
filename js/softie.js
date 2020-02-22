952bd53143bb55860cc2eb143fa9efdf3b1fd3e9
levit123 <30578168+levit123@users.noreply.github.com>
2020-02-22 08:35:31 -0800

    discography + javascript
    -added the discography section
    -added links to navbar
    -tweaked layout of page
    -tweaked styling of page
    -added javascript for opening links
    -added album covers to "images" folder

---
+++ js/softie.js
@@ -0,0 +1,25 @@
+/*functions for opening links to specific albums in the Discography section*/
+
+function OpenBlondeBoi() {
+	window.open("https://soundcloud.com/softie_love/sets/blondeboi-volume-1");
+}
+
+function OpenSteamedMilk() {
+	window.open("https://soundcloud.com/softie_love/sets/steamed-milk");
+}
+
+function OpenCigaretteKiss() {
+	window.open("https://soundcloud.com/softie_love/sets/cigarette-kiss");
+}
+
+function OpenWhereCloudsGoToSleep() {
+	window.open("https://soundcloud.com/softie_love/sets/where-clouds-go-to-sleep-full-beattape");
+}
+
+function OpenSoundcloud() {
+	window.open("https://soundcloud.com/softie_love");
+}
+
+function OpenSpotify() {
+	window.open("https://open.spotify.com/artist/2KDEZe6Bd68qxSYbxsRvim");
+}
